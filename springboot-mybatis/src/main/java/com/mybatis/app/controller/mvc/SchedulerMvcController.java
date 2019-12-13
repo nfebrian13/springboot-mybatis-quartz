8751f563af63c3c903f4205691294e1546182ed7
nfebrian13 <nana.febriana@sinarmasmsiglife.co.id>
2019-12-13 14:52:42 +0700

    commit

--- springboot-mybatis/src/main/java/com/mybatis/app/controller/mvc/SchedulerMvcController.java
+++ springboot-mybatis/src/main/java/com/mybatis/app/controller/mvc/SchedulerMvcController.java
@@ -6,21 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mybatis.app.instance.service.impl.SchedulerServiceImpl;
 import com.mybatis.app.model.SchedulerJob;
 
 @Controller
+@RequestMapping("/scheduler")
 public class SchedulerMvcController {
 
 	@Autowired
 	private SchedulerServiceImpl service;
 
-	@GetMapping("/schedulerlist")
+	@GetMapping("/create")
+	public String create(Model model) {
+		return "scheduler/create_scheduler";
+	}
+
+	@GetMapping("/list")
 	public String listJob(Model model) {
 		List<SchedulerJob> listJob = service.schedulerJobMvcList();
 		System.out.println("scheduluerJob size: " + listJob.size());
 		model.addAttribute("listJobs", listJob);
 		return "scheduler/list_scheduler";
 	}
+
 }
