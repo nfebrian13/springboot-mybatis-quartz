5f97d101e28f76d5b4f0459b47cc32c1625d3f92
nfebrian13 <febrian.nana@gmail.com>
2020-01-12 10:41:15 +0700

    commit

--- springboot-mybatis/src/main/java/com/mybatis/app/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/mybatis/app/SpringbootMybatisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMybatisApplication {
 
 	public static void main(String[] args) {
+		System.out.println("SpringbootMybatisApplication..");
 		SpringApplication.run(SpringbootMybatisApplication.class, args);
 	}
 }
